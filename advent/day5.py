348527905054e33d6f9faeabd0495211f5ec07a3
Chris Hunt <chris.hunt@hudl.com>
2023-12-05 09:00:24 +0000

    feat: day 5, part 1 complete. Part 2 not working

--- advent/day5.py
+++ advent/day5.py
@@ -8,63 +8,87 @@ class Solution(utils.DaySolution):
 
     def solve_part_1(self):
         seeds = self.data["seeds"]
+        return self.get_min_location(seeds)
+
+    def solve_part_2(self):
+        seed_ranges = self.data["seeds"]
+        min_for_ranges = []
+        for i in range(0, len(seed_ranges) - 1, 2):
+            seed_range = [
+                *range(seed_ranges[i], seed_ranges[i] + seed_ranges[i + 1] + 1)
+            ]
+            min_for_ranges.append(self.get_min_location(seed_range))
+        return min(min_for_ranges)
+
+    def get_min_location(self, seeds: List[int]) -> int:
         maps = self.data["maps"]
         locations = []
-        
+
         for location in seeds:
-            print(location)
             for map in maps:
+                for map_entry in map["map"]:
+                    if (
+                        map_entry["source_start"]
+                        <= location
+                        <= map_entry["source_start"] + map_entry["length"]
+                    ):
+                        location = map_entry["dest_start"] + (
+                            location - map_entry["source_start"]
+                        )
+                        break
                 if location in map["map"]:
                     location = map["map"][location]
-                print(location)
-            print("----")
             locations.append(location)
         return min(locations)
 
-    def solve_part_2(self):
-        pass
-
     def parse_data(self, data: List[str]) -> any:
         maps = []
-        
+
         source = None
         dest = None
-        map = {}
-        
+        map = []
+
         for i, line in enumerate(data):
             if i == 0:
                 continue
-            
+
             if line == "":
                 if source is not None:
-                    maps.append({
-                        "source": source,
-                        "dest": dest,
-                        "map": map,
-                    })
+                    maps.append(
+                        {
+                            "source": source,
+                            "dest": dest,
+                            "map": map,
+                        }
+                    )
                     source = None
                     dest = None
-                    map = {}
+                    map = []
                 continue
             if line[0].isdigit():
-                source_start, dest_start, length = line.split()
-                for i in range(0, int(length)):
-                    map[int(dest_start) + i] = int(source_start) + i
-                
+                dest_start, source_start, length = line.split()
+                map.append(
+                    {
+                        "source_start": int(source_start),
+                        "dest_start": int(dest_start),
+                        "length": int(length),
+                    }
+                )
+
             else:
                 parts = line.split(" ")[0].split("-")
                 source = parts[0]
                 dest = parts[2]
-            
-        maps.append({
-            "source": source,
-            "dest": dest,
-            "map": map,
-        })
-        
+
+        maps.append(
+            {
+                "source": source,
+                "dest": dest,
+                "map": map,
+            }
+        )
+
         return {
-            "seeds": [ int(i) for i in data[0].split(":")[1].split()],
+            "seeds": [int(i) for i in data[0].split(":")[1].split()],
             "maps": maps,
         }
-
-
--- advent/tests/day5_test.py
+++ advent/tests/day5_test.py
@@ -5,11 +5,22 @@ class TestDay(TestDayBase):
     __test__ = True
     day = 5
     result_part_1 = 35
-    result_part_2 = None
+    result_part_2 = 46
 
     def test_it_gets_data(self):
         solver = self.puzzle.Solution(test=True)
-        self.assertEqual(solver.data["seeds"], [79, 14,55,13])
+        self.assertEqual(solver.data["seeds"], [79, 14, 55, 13])
         self.assertEqual(len(solver.data["maps"]), 7)
-        self.assertEqual(len(solver.data["maps"][6]["map"].keys()), 41)
-        
+        self.assertEqual(len(solver.data["maps"][6]["map"]), 2)
+
+    def test_part1(self):
+        if self.result_part_1 is None:
+            self.skipTest("No result for part 1")
+        solver = self.puzzle.Solution(test=True)
+        self.assertEqual(solver.solve_part_1(), self.result_part_1)
+
+    def test_part2(self):
+        if self.result_part_2 is None:
+            self.skipTest("No result for part 2")
+        solver = self.puzzle.Solution(test=True, part_2=True)
+        self.assertEqual(solver.solve_part_2(), self.result_part_2)
